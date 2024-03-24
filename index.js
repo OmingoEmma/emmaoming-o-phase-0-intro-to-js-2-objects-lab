8f837e5881b8e6feaa17abc334cf50084833c875
Emma <emmaomingo@gmail.com>
2024-03-24 22:35:26 +0300

    Objects & Arrays

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+// index.js
+const employee = {
+    name: 'Sam',
+  };
+  
+  const updateEmployeeWithKeyAndValue = (obj, key, value) => ({
+    ...obj,
+    [key]: value,
+  });
+  
+  const destructivelyUpdateEmployeeWithKeyAndValue = (obj, key, value) => {
+    obj[key] = value;
+    return obj;
+  };
+  
+  const deleteFromEmployeeByKey = (obj, key) => {
+    const newObj = { ...obj };
+    delete newObj[key];
+    return newObj;
+  };
+  
+  const destructivelyDeleteFromEmployeeByKey = (obj, key) => {
+    delete obj[key];
+    return obj;
+  };
+  
+  module.exports = {
+    employee,
+    updateEmployeeWithKeyAndValue,
+    destructivelyUpdateEmployeeWithKeyAndValue,
+    deleteFromEmployeeByKey,
+    destructivelyDeleteFromEmployeeByKey,
+  };
